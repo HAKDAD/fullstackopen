dd944c52e49e56c1d657e53b637888ed28cc713f
Mr_Kali_Hacker <kottevamsi38@gmail.com>
2022-09-11 11:38:21 +0530

    Update App.js

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -4,44 +4,53 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all > 0 ? roundToTwo((good - bad) / all) : 0;
+  const positive = all > 0 ? roundToTwo((good / all) * 100) : 0;
+
+  if (all === 0) {
+    return <div>No feedback given</div>;
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
+  );
+};
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+// Round to two decimal places
+const roundToTwo = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
+
 const App = () => {
-  const anecdotes = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
-  ];
-
-  const [selected, setSelected] = useState(0);
-
-  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
-
-  const addVote = () => {
-    const pointsCopy = [...points];
-    pointsCopy[selected] += 1;
-    setPoints(pointsCopy);
-  };
-
-  const bestAnecdoteIndex = points.indexOf(Math.max(...points));
+  // Save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <div>{anecdotes[selected]}</div>
-      <div>has {points[selected]} points</div>
-      <Button handleClick={() => addVote()} text="vote" />
-      <Button
-        handleClick={() =>
-          setSelected(Math.floor(Math.random() * anecdotes.length))
-        }
-        text="next anecdote"
-      />
-      <h1>Anecdote with most votes</h1>
-      <div>{anecdotes[bestAnecdoteIndex]}</div>
-      <div>has {points[bestAnecdoteIndex]} votes</div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
